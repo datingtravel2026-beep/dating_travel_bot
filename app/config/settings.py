f3d0fef0630e7ec1df2c0a3037a89f138f5a13af
datingtravel2026-beep <datingtravel2026@gmail.com>
2026-05-06 19:20:13 +0300

    Update settings.py

--- app/config/settings.py
+++ app/config/settings.py
@@ -10,3 +10,13 @@ CHANNEL_ID = int(os.getenv("CHANNEL_ID", 0))
 CHAT_ID = int(os.getenv("CHAT_ID", 0))
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL not set")
+
+# фикс под asyncpg
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+asyncpg://")
+
+elif DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://")
