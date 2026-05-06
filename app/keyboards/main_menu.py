71a6bab395891e467269b7675c13071f96bb3b0b
datingtravel2026-beep <datingtravel2026@gmail.com>
2026-05-06 19:05:19 +0300

    Update main_menu.py

--- app/keyboards/main_menu.py
+++ app/keyboards/main_menu.py
@@ -1,1 +1,12 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+
+def main_menu():
+    return ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="Профиль"), KeyboardButton(text="Поиск")],
+            [KeyboardButton(text="Анонимка"), KeyboardButton(text="Магазин")],
+            [KeyboardButton(text="О нас")]
+        ],
+        resize_keyboard=True
+    )
