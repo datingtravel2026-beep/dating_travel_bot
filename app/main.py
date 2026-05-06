efa5a910d827d4a864ee28da07bd1a1c95c10709
datingtravel2026-beep <datingtravel2026@gmail.com>
2026-05-06 19:05:58 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
+import asyncio
 
+from app.config.loader import dp, bot
+from app.handlers import start, registration
+
+
+async def main():
+    dp.include_router(start.router)
+    dp.include_router(registration.router)
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
