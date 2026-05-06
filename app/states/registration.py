9b2969e750beacebbf280de3535638dac8ad4de9
datingtravel2026-beep <datingtravel2026@gmail.com>
2026-05-06 19:04:53 +0300

    Update registration.py

--- app/states/registration.py
+++ app/states/registration.py
@@ -1,1 +1,11 @@
+from aiogram.fsm.state import StatesGroup, State
 
+
+class Registration(StatesGroup):
+    name = State()
+    age = State()
+    gender = State()
+    looking_for = State()
+    city = State()
+    photo = State()
+    confirm = State()
